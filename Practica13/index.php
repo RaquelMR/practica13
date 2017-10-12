9b3f396f671af58a1bc5e488e65cafe9f18c2c8b
Raquel <30847083+RaquelMR@users.noreply.github.com>
2017-10-12 15:31:25 -0500

    Add files via upload

---
+++ Practica13/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="procesar.php" method="post" name="datospersonales">
+  Nombre:<br>
+  <input type="text" name="nombreAlumno" value="" placeholder="Introduce Nombre">
+  <br>
+  Apellido:<br>
+  <input type="text" name="apellidosAlumno" value="" placeholder="Introduce Apellidos">
+  <br><br>
+  Edad:<br>
+  <input type="text" name="edadAlumno" value="" placeholder="Introduce Edad">
+  <br><br>
+  <input type="submit" value="Guardar">
+</form> 
+<p>Raquel Alejandra Martinez Ramirez</p>
+</body>
+</html>
---
+++ Practica13/procesar.php
@@ -0,0 +1,9 @@
+<head>
+	<meta charset="utf-8">
+</head>
+<?php
+$nombre=$_POST['nombreAlumno'];
+$apellidos=$_POST['apellidosAlumno'];
+$edad=$_POST['edadAlumno'];
+echo 'Tu nombre es:'.$nombre.' '.$apellidos.' tu edad es : '.$edad.' años de edad';
+?>
